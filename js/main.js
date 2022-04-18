d51ec6e885c1396dab165fa939aede834c75cb11
Optimoze <jyd15@naver.com>
2022-04-18 09:12:50 +0900

    s

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $(function(){
         dots : true,
         autoplay : true,
         autoplaySpeed : 5000,
-        pauseOnHover : false,
+        pauseOnHover : true,
     });
 
     $(".regular").slick({
@@ -29,7 +29,8 @@ $(function(){
         autoplay : true,
         autoplaySpeed : 2000,
         slidesToShow: 5,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        pauseOnHover : true,
     });
 })//ready 슬라이드 slick
 
@@ -74,20 +75,36 @@ $(function(){
 })//ready 크기에 따른 슬라이드 이미지 변경
 
 $(function(){
+    var btn_on = 0;
+    $('#btn1').click(function(){
+        if (btn_on == 0) {
+            $(this).addClass('on');
+            $('#nav .depth1').slideDown();
+            btn_on = 1;
+        } 
+        else {
+            $(this).removeClass('on');
+            $('#nav .depth1').slideUp();
+            btn_on = 0;
+        }
+    })
+})//readey 네비 버튼
+
+$(function(){
     //새로고침 되었을 때 화면의 크기에 맞게
-    if( $(window).width() < 1151 ){
-        $('#nav img').attr('src','image/logo1_1150.png');
+    if( $(window).width() < 1031 ){
+        $('#nav .depth1').hide();
     }
     else {
-        $('#nav img').attr('src','image/logo1.png');
+        $('#nav .depth1').show();
     }
     //윈도우 창 크기 조정함에 따른 화면의 크기에 맞게
     $(window).resize(function(){
-        if( $(window).width() < 1151 ){
-            $('#nav img').attr('src','image/logo1_1150.png');
+        if( $(window).width() < 1031 ){
+            $('#nav .depth1').hide()
         }
         else {
-            $('#nav img').attr('src','image/logo1.png');
+            $('#nav .depth1').show();
         }
     })//resize
-})//ready 크기에 따른 로고 이미지 변경
+})//ready 크기에 따른 네비 등장 여부
